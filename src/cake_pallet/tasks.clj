014b6b6fdfc7c35eecb90c3e980d6b1c50101d66
David Santiago <david.santiago@gmail.com>
2010-10-14 22:10:25 -0500

    Finally works right.

--- src/cake_pallet/tasks.clj
+++ src/cake_pallet/tasks.clj
@@ -10,6 +10,7 @@
    For a list of tasks
      cake pallet help"
   (bake (:require [clojure.string :as str])
+	(:require pallet.main)
 	[options (:pallet *opts*)]
 	(do
 	  (try
@@ -18,9 +19,4 @@
 	    (catch java.io.FileNotFoundException e
 	      (println "Error loading pallet: " (.getMessage e))
 	      (println "You need to have pallet as a project dependency")
-	      (println "or installed in ~/.cake/project.clj")))
-
-	  (if-let [m (ns-resolve 'pallet.main (symbol "-main"))]
-	    (apply m options)
-	    (do
-	      (println "failed to resolve " 'pallet.main (symbol "-main")))))))
+	      (println "or installed in ~/.cake/project.clj"))))))
