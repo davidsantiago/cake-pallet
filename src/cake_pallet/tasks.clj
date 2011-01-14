3c037efca8f3886391fbaf18f20674388580fd61
David Santiago <david.santiago@gmail.com>
2011-01-14 16:22:17 -0600

    Update to use pallet-task instead of -main.

--- src/cake_pallet/tasks.clj
+++ src/cake_pallet/tasks.clj
@@ -7,14 +7,14 @@
 (deftask pallet
   "Launch pallet tasks from the command line. Use 'cake pallet help' for tasks."
   (bake (:require [clojure.string :as str])
-	(:require pallet.main)
-	[options (concat (:pallet *opts*)
-			 (rest (drop-while #(not= "--" %) *command-line-args*)))]
-	(do
-	  (try
-	    (require 'pallet.main)
-	    (apply pallet.main/-main "-project-options" (pr-str *project*) options)
-	    (catch java.io.FileNotFoundException e
-	      (println "Error loading pallet: " (.getMessage e))
-	      (println "You need to have pallet as a project dependency")
-	      (println "or installed in ~/.cake/project.clj"))))))
+        (:require pallet.main)
+        [options (concat (:pallet *opts*)
+                         (rest (drop-while #(not= "--" %) *command-line-args*)))]
+        (do
+          (try
+            (require 'pallet.main)
+            (apply pallet.main/pallet-task "-project-options" (pr-str *project*) options)
+            (catch java.io.FileNotFoundException e
+              (println "Error loading pallet: " (.getMessage e))
+              (println "You need to have pallet as a project dependency")
+              (println "or installed in ~/.cake/project.clj"))))))
