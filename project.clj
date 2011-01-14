60a9c5dd2bf29fd1f0f605772473be2448d7087f
David Santiago <david.santiago@gmail.com>
2011-01-14 15:52:13 -0600

    Update version to match Pallet's.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cake-pallet "0.3.0-SNAPSHOT"
+(defproject cake-pallet "0.4.0-SNAPSHOT"
   :description "A task for controlling pallet."
   :dependencies [[clojure "1.2.0"]
-		 [org.cloudhoist/pallet "0.4.0-SNAPSHOT"]]
+                 [org.cloudhoist/pallet "0.4.0-SNAPSHOT"]]
   :tasks [cake-pallet.tasks])
